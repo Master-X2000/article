79525612307512e62b0eadcc2e88f330f91c5c59
Master-X2000 <145823548+Master-X2000@users.noreply.github.com>
2025-07-10 02:20:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Article preview component solution
+
+This is a solution to the [Article preview component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- See the social media share links when they click the share icon
+
+### Links
+
+- Solution URL: [View](https://github.com/master-x2000/article)
+- Live Site URL: [View](https://master-x2000.github.io/article/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JS Event Listeners
+
+### Continued development
+
+I wiill have to continue working on my DOMS and event listener.
+
+## Author
+
+- Website - [Odezime Excel](https://www.your-site.com)
+- Frontend Mentor - [@Master-x2000](https://www.frontendmentor.io/profile/master-x2000)
+- Twitter - [@yourusername](https://www.twitter.com/EOdezime66661)
---
+++ js/main.js
@@ -0,0 +1,52 @@
+const shareBtn = document.querySelector(".share_icon");
+const shareBtnArrow = document.querySelector(".share_icon svg");
+const shareTo = document.querySelector(".share");
+const writer = document.querySelector(".writer");
+shareTo.style.display = "none";
+let shareToState = "none";
+
+shareTo.addEventListener("click", (event) => {
+    shareTo.style.display = "flex";
+    shareBtn.style.backgroundColor = "#6d7f97";
+    shareBtnArrow.style.fill = "#fff";
+    shareToState = "flex";
+    writer.classList.add("hide_writer");
+});
+
+shareBtn.addEventListener("click", (event) => {
+    let target = event.target;
+    if(shareToState === "none") {
+        shareTo.style.display = "flex";
+        shareBtn.style.backgroundColor = "#6d7f97";
+        shareBtnArrow.style.fill = "#fff";
+        shareToState = "flex";
+        writer.classList.add("hide_writer");
+    } else {
+        shareTo.style.display = "none";
+        shareBtn.style.backgroundColor = "#ecf2f8";
+        shareBtnArrow.style.fill = "#6d7f97";
+        writer.classList.remove("hide_writer");
+        shareToState = "none";
+    }
+        
+    document.addEventListener("click", (event) => {
+        if (event.target !== target  &&
+            event.target.className !== "share" &&
+            event.target.className !== "share_to" &&
+            event.target.className !== "not-remove" &&
+            event.target.className !== "icons") {
+            shareTo.style.display = "none";
+            shareBtn.style.backgroundColor = "#ecf2f8";
+            shareBtnArrow.style.fill = "#6d7f97";
+            writer.classList.remove("hide_writer");
+            shareToState = "none";
+
+        } else {
+            shareTo.style.display = "flex";
+            shareBtn.style.backgroundColor = "#6d7f97";
+            shareBtnArrow.style.fill = "#fff";
+            shareToState = "flex";
+            writer.classList.add("hide_writer");
+        }
+    });
+});            
